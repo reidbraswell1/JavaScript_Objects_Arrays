d40de524c360cc2ebad669695bd132514c99fcf8
Reid Braswell <reidbras@gmail.com>
2021-07-03 16:36:55 -0500

    Adding validationa and controller functions.

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 console.log("Hello World!\n==========\n");
 
+function validateForm() {
+    console.log("Begin validateForm()");
+    console.log(`Array value = ${array.value}`);
+    let arrayString = (array.value);
+    let arrayNum = arrayString.split(",");
+    console.log(`Array = ${arrayNum}`);
+    return false;
+}
+
+function controller(obj) {
+    console.log("Begin controller");
+}
+
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
