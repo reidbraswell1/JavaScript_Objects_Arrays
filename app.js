236a3dfb151ff1852b986c4154ace643390ca907
Reid Braswell <reidbras@gmail.com>
2021-07-03 17:52:30 -0500

    Adding globals and validation

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 console.log("Hello World!\n==========\n");
 
+// Globals
+const errorElementId = "error";
+const error = document.getElementById(errorElementId);
+
 function validateForm() {
     console.log("Begin validateForm()");
+
+    error.innerText="";
+    error.style.color = "black";
+
     console.log(`Array value = ${array.value}`);
     let arrayString = (array.value);
     let arrayNum = arrayString.split(",");
     console.log(`Array = ${arrayNum}`);
+    arrayNum.forEach(parseArray)
+    function parseArray(element,index,array) {
+        if(isNaN(element)) {
+            console.log(`Invalid array element "${element}" at index ${index}`);
+            error.innerText=`Invalid array element "${element}" at index ${index}`;
+            error.style.color="red";
+        }
+        
+    };
     return false;
 }
 
