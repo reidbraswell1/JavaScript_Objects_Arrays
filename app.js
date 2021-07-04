1cde6999c0ebc96b66cce7489c446a513cb9cb30
Reid Braswell <reidbras@gmail.com>
2021-07-04 16:13:19 -0500

    Updated book processing

--- app.js
+++ app.js
@@ -1,68 +1,77 @@
 console.log("Hello World!\n==========\n");
 
-// Globals
-const errorElementId = "error";
-const error = document.getElementById(errorElementId);
+// Constants
+function constants() {
+  const DOM_ElementIDs = {
+    errorElementId: "error",
+    errorStyleColor: "red",
+    normalStyleColor: "black",
+    errorId: function () {
+      let id = document.getElementById(this.errorElementId);
+      return id;
+    },
+  };
+  return DOM_ElementIDs;
+}
 
-
-let book = {
-        // Return an array of strings containing book info
-        bookInfo: function () {
-            let bookInfo = [];
-            book.forEach(function (value, index,array) {
-              let bookString = `${book.title}, ${book.pages} pages, read ${book.readCount} times.`;
-              bookInfo.append(bookString);  
-            })
-            return bookInfo;
-        },
-        bookAdd(bookTitle, bookPages, bookNbrTimesRead) {
-            book.title=bookTitle,
-            book.pages=bookPages,
-            book.readCount=bookNbrTimesRead;
-        }
+// Book object
+function bookProcessing(titleP="", pagesP=0, readCountP=0, bookInfoP = []) {
+  if (titleP.length == 0 || pagesP <= 0 ||  readCountP <= 0) {
+    return null;
+  }
+  let book = {
+    title: titleP,
+    pages: pagesP,
+    readCount: readCountP,
+    bookInfoAcumulator: bookInfoP,
+    // Return an array of strings containing book info
+    bookInfo: function () {
+      let bookString = `${this.title}, ${this.pages} pages, read ${this.readCount} times.`;
+      bookInfoAcumulator.push(bookString);
+      return bookInfoP;
+    },
+  };
+  return book;
 }
 
 function validateForm() {
-    console.log("Begin validateForm()");
+  console.log("Begin validateForm()");
 
-    error.innerText="";
-    error.style.color = "black";
+  constants().errorId().innerText = "";
+  constants().errorId().style.color = constants().normalStyleColor;
 
-    console.log(`Array value = ${array.value}`);
-    let arrayString = (array.value);
-    let arrayNum = arrayString.split(",");
-    console.log(`Array = ${arrayNum}`);
-    arrayNum.forEach(parseArray)
-    function parseArray(element,index,array) {
-        if(isNaN(element)) {
-            console.log(`Invalid array element "${element}" at index ${index}`);
-            error.innerText=`Invalid array element "${element}" at index ${index}`;
-            error.style.color="red";
-        }
-        
-    };
-    return false;
+  console.log(`Array value = ${array.value}`);
+  let arrayString = array.value;
+  let arrayNum = arrayString.split(",");
+  console.log(`Array = ${arrayNum}`);
+  arrayNum.forEach(parseArray);
+  function parseArray(element, index, array) {
+    if (isNaN(element)) {
+      console.log(`Invalid array element "${element}" at index ${index}`);
+      error.innerText = `Invalid array element "${element}" at index ${index}`;
+      error.style.color = constants().errorStyleColor;
+    }
+  }
+  return false;
 }
 
 function controller(obj) {
-    console.log("Begin controller");
+  console.log("Begin controller");
 }
 
 // Function to add or get the books from the book object.
-function processBook (operation, bookTitle, bookPages, bookNbrTimesRead) {
-    switch(operation) {
-        case "post":
-            book.bookAdd(bookTitle, bookPages, bookNbrTimesRead);
-            break;
-        case "get":
-            return book.bookInfo();
-            break;
-    }
+function processBook(operation, bookTitle, bookPages, bookNbrTimesRead) {
+  switch (operation) {
+    case "post":
+      book.bookAdd(bookTitle, bookPages, bookNbrTimesRead);
+      break;
+    case "get":
+      return book.bookInfo();
+      break;
+  }
 }
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
-
-
